d5646e2b647e872af28f31ed30ac71d6ddc4b8a4
Stephanie Parker <stephanieparker@MacBook-Pro-3.local>
2018-11-13 16:11:03 +0000

    redirected to play and stored names in session

--- app.rb
+++ app.rb
@@ -1,13 +1,22 @@
 require 'sinatra/base'
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/players' do
     erb(:player_form)
   end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
+    erb(:player_names)
+  end
+
   post '/names' do
     p params
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
-    "#{@player_1} and #{@player_2}"
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect to('/play')
   end
   run! if app_file == $0
 end
